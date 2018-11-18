8bcfdc2f334c34066bc99c00c1dd21eeeeb05ba6
Brendan Murphy <brendan_murphy@harvard.edu>
2018-11-18 16:19:31 -0500

    Added loading daly 2000 country data

--- js/load_data.js
+++ js/load_data.js
@@ -31,18 +31,35 @@ function processDALYRow(d) {
     }
 }
 
+const DALYStringColumns = ['sex', 'cause_of_death_0', 'cause_of_death_1', 'cause_of_death_2', 'cause_of_death_3',
+    'cause_of_death_4'];
+
+function processDALYCountryRow(d) {
+    for (let key in d) {
+        if (!DALYStringColumns.includes(key)) {
+            if (d[key] === '.') {
+                d[key] = 0;
+            }
+            d[key] = +d[key];
+        }
+    }
+    return d;
+}
+
 
 queue()
     .defer(d3.csv, 'data/DALY_Global_2000.csv', processDALYRow)
     .defer(d3.csv, 'data/DALY_Global_2010.csv', processDALYRow)
     .defer(d3.csv, 'data/DALY_Global_2015.csv', processDALYRow)
     .defer(d3.csv, 'data/DALY_Global_2016.csv', processDALYRow)
+    .defer(d3.csv, 'data/DALY-2000-country-all.csv', processDALYCountryRow)
     .await(printData);
 
 
-function printData(error, daly_2000, daly_2010, daly_2015, daly_2016) {
+function printData(error, daly_2000, daly_2010, daly_2015, daly_2016, daly_country_2000) {
     console.log(daly_2000);
     console.log(daly_2010);
     console.log(daly_2015);
     console.log(daly_2016);
+    console.log(daly_country_2000);
 }
