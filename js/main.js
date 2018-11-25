7cc520c4366de8e498ad0dc619dcb651506bde73
Brendan <bmurphy592@gmail.com>
2018-11-25 17:07:02 -0500

    Updated beating heart to use a different icon for inactivity. Fixed selection dots and tooltips

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 //SCROLL LIBRARY
 $('.mainbag').viewScroller({
                 useScrollbar: false,
-                changeWhenAnim: false
+                changeWhenAnim: false,
             });
 
-//LEFT NAVIGATION DOTS 
-var windowWidth = $( window ).width() - 100;
-
-            $(".navigation-dot").mousemove(function(event){
-              var text = event.target.id;
-              $(".navigation-tooltip").css("top",event.pageY-40);	
-              $(".navigation-tooltip").css("left",windowWidth);	
-              $(".navigation-tooltip").css("display","block");
-              $(".navigation-tooltip").html(text);
-            });
-            
-            $(".navigation-dot").mouseout(function(){
-              $(".navigation-tooltip").css("display","none");
-            });
+//NAVIGATION DOTS
+var windowWidth = $( window ).width() - 200;
+    $(".navigation-dot").mousemove(function(event) {
+        let text = $(event.target).data('title');
+        $(".navigation-tooltip").css({
+            top: event.pageY - 40,
+            left: windowWidth,
+            display: "block",
+            width: 175,
+        }).html(text)
+    }).mouseout(function(){
+        $(".navigation-tooltip").css("display","none");
+    });
 
 $(".count-1").animateNumber({ number: 17.9});
 $(".count-2").animateNumber({ number: 85});
