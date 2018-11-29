23ce4dceeddf5f5e13c8dd407386fdd7bfee546c
Ka Ning Yip <kaningyip@gmail.com>
2018-11-28 23:18:41 -0500

    fixed the axes

--- js/cholesterol_line.js
+++ js/cholesterol_line.js
@@ -1,6 +1,6 @@
-var marginC = {top: 60, right: 100, bottom: 20, left: 80},
-    widthC = 800 - marginC.left - marginC.right,
-    heightC = 600 - marginC.top - marginC.bottom;
+var marginC = {top: 20, right: 80, bottom: 20, left: 80},
+    widthC = 700 - marginC.left - marginC.right,
+    heightC = 500 - marginC.top - marginC.bottom;
 
 //to do: define scales; initVis; enter, update, exit based on user selection
 //sources of data: js/load_data_cholesterol.js
@@ -9,30 +9,32 @@ var marginC = {top: 60, right: 100, bottom: 20, left: 80},
 var parseYear = d3.timeParse("%Y");
 var formatYear = d3.timeFormat("%Y");
 
-var xC = d3.scaleTime()
+var x = d3.scaleTime()
     .range([0, widthC]);
 
-var yC = d3.scaleLinear()
-    .range([heightC, 0]);
+var y = d3.scaleLinear()
+    .range([heightC, marginC.top]);
 
 var mean_cholesterol = [];
 var raise_cholesterol5 = [];
 
 // Define the line
 var lineC = d3.line()
-    .x(function(d) { return xC(+d.Year); })
-    .y(function(d) { return yC(+d.Cholesterol); });
+    .x(function(d) { return x(+d.Year); })
+    .y(function(d) { return y(+d.Cholesterol); });
 
 // Create the svg canvas in the "graph" div
 var svgC = d3.select("#chart-area-6")
     .append("svg")
-    .style("width", widthC + marginC.left + marginC.right + "px")
-    .style("height", heightC + marginC.top + marginC.bottom + "px")
+    .style("width", widthC + marginC.left + marginC.right)
+    .style("height", heightC + marginC.top + marginC.bottom)
     .attr("width", widthC + marginC.left + marginC.right)
     .attr("height", heightC + marginC.top + marginC.bottom)
     .append("g")
     .attr("transform","translate(" + marginC.left + "," + marginC.top + ")")
     .attr("class", "svg");
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 // Import the CSV data
 d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, data) {
@@ -68,37 +70,38 @@ d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, dat
             return d.Country;
         })
         .rollup(function(leaves){
-            var max = d3.max(leaves, function(d){
+            var cholesterol = d3.max(leaves, function(d){
                 return d.Cholesterol
             })
             var gender = d3.nest().key(function(d){
                 return d.Gender
             })
                 .entries(leaves);
-            return {max:max, gender:gender};
+            return {cholesterol:cholesterol, gender:gender};
         })
         .entries(mean_cholesterol);
 
 
     // Scale the range of the data
-    xC.domain(d3.extent(mean_cholesterol, function(d) { return +d.Year; }));
-    yC.domain([0, d3.max(mean_cholesterol, function(d) { return d.Cholesterol; })]);
+    x.domain(d3.extent(mean_cholesterol, function(d) { return +d.Year; }));
+    y.domain([d3.min(mean_cholesterol, function(d) { return +d.Cholesterol; }),
+        d3.max(mean_cholesterol, function(d) { return +d.Cholesterol; })]);
 
     // Set up the x axis
-    var xAxisC = svgC.append("g")
+    var xAxis = svgC.append("g")
         .attr("transform", "translate(0," + heightC + ")")
         .attr("class", "x axis")
-        .call(d3.axisBottom(xC)
+        .call(d3.axisBottom(x)
             .tickSize(0, 0)
             .tickFormat(d3.timeFormat("%Y"))
             .tickSizeInner(0)
             .tickPadding(10));
 
     // Add the Y Axis
-    var yAxisC = svgC.append("g")
+    var yAxis = svgC.append("g")
         .attr("class", "y axis")
-        .call(d3.axisLeft(yC)
-            .ticks(5)
+        .call(d3.axisLeft(y)
+            .ticks(10)
             .tickSizeInner(0)
             .tickPadding(6)
             .tickSize(0, 0));
@@ -129,23 +132,6 @@ d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, dat
             return d.key;
         });
 
-    // Create a dropdown for gender
-    var genderMenu = d3.select("#gender");
-
-    genderMenu
-        .data(nest)
-        .append("select")
-        .selectAll("option")
-        .data(function(d) { return d.value.gender; })
-        .enter()
-        .append("option")
-        .attr("value", function(d){
-            return d.key;
-        })
-        .text(function(d){
-            return d.key;
-        })
-
 
     // Function to create the initial graph
     var initialGraph = function(country){
@@ -161,10 +147,7 @@ d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, dat
             })
             .enter()
             .append("g")
-            .attr("class", "CountryGroups")
-            .each(function(d){
-                yC.domain([0, d.value.max])
-            });
+            .attr("class", "CountryGroups");
 
 
         var initialPath = selectCountryGroups.selectAll(".line")
@@ -178,7 +161,43 @@ d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, dat
             })
             .attr("class", "line")
             .style("stroke-dasharray", function(d){
-                return (d.key == "Male") ? ("5, 5") : ("0, 0")});
+                return (d.key == "Male") ? ("3, 3") : ("0, 0")});
+
+        var legendVals1 = d3.scaleOrdinal()
+            .domain(function(d) { return d.value.gender; })
+            .range(["#1F77B4", "#FF7F0E"]);
+
+
+        //D3 Vertical Legend//////////////////////////
+        var legend3 = svgC.selectAll('.legend3')
+            .data(legendVals1.domain())
+            .enter().append('g')
+            .attr("class", "legends3")
+            .attr("transform", function (d, i) {
+                {
+                    return "translate(0," + i * 20 + ")"
+                }
+            })
+
+        legend3.append('rect')
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", 10)
+            .attr("height", 10)
+            .style("fill", function (d, i) {
+                return color(i)
+            })
+
+        legend3.append('text')
+            .attr("x", 20)
+            .attr("y", 10)
+            //.attr("dy", ".35em")
+            .text(function (d, i) {
+                return d
+            })
+            .attr("class", "textselected")
+            .style("text-anchor", "start")
+            .style("font-size", 15)
 
 
      //   document.getElementsByClassName("cholesterol-prevalence-1")[0].innerHTML = "In 2008, the prevalence of high " +
@@ -200,13 +219,10 @@ d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, dat
         // Select all of the grouped elements and update the data
         var selectCountryGroups = svgC.selectAll(".CountryGroups")
             .data(selectCountry)
-            .style("color", function(d, i) { return i ? null : "red"; })
-            .each(function(d){
-                yC.domain([0, d.value.max])
-            });
+            .style("color", function(d, i) { return i ? null : "red"; });
 
         // Select all the lines and transition to new positions
-        selectCountryGroups.selectAll("path.line")
+        selectCountryGroups.selectAll(".line")
             .data(function(d) { return d.value.gender; },
                 function(d){ return d.key; })
             .transition()
@@ -219,7 +235,7 @@ d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, dat
         d3.select(".y")
             .transition()
             .duration(1500)
-            .call(d3.axisLeft(yC)
+            .call(d3.axisLeft(y)
                 .ticks(5)
                 .tickSizeInner(0)
                 .tickPadding(6)
@@ -244,24 +260,5 @@ d3.csv("data/mean-total-blood-cholesterol-age-adjusted.csv", function(error, dat
         updateGraph(selectedCountry);
 
     });
-
-    // Change color of selected line when year dropdown changes
-    genderMenu.on('change', function(){
-
-        // Find which year was selected
-        var selectedGender = d3.select(this)
-            .select("select")
-            .property("value")
-
-        // Change the class of the matching line to "selected"
-        var selLine = svg.selectAll(".line")
-        // de-select all the lines
-            .classed(".selected", false)
-            .filter(function(d) {
-                return +d.key === +selectedGender
-            })
-            // Set class to selected for matching line
-            .classed(".selected", true)
-    })
 
 });
