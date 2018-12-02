f41b25deeaf32c81dd1ad119e9082b9dee2b49d8
Brendan Murphy <brendan_murphy@harvard.edu>
2018-12-02 17:20:53 -0500

    Added world legend to bar chart

--- js/Prevalence.js
+++ js/Prevalence.js
@@ -1,10 +1,16 @@
-BarChart = function(_parentElement, _data) {
+BarChart = function(_parentElement, _data, _legendElement, _legendData) {
     this.parentElement = _parentElement;
     this.data = _data;
     this.filteredData = this.data;
     this.selectedCategory = 'overweight';
     this.transitionDuration = 500;
     this.reversed = false;
+    this.textFriendlyCategories = {
+        overweight: "being overweight",
+        obesity: "obesity",
+        physicallyInactive: "physical inactivity"
+    };
+    this.mapLegend = new WorldLegend(_legendElement, _legendData, filterBar);
 
     this.initVis();
 };
@@ -41,13 +47,6 @@ BarChart.prototype.initVis = function () {
         .attr('transform', 'translate(0,' + vis.height + ')')
         .attr('class', 'x-axis axis');
 
-    // vis.xAxisLabel = vis.svg.append('text')
-    //     .attr('x', vis.width)
-    //     .attr('y', vis.height)
-    //     .attr('text-anchor', 'end')
-    //     .attr('class', 'label')
-    //     .text('Blood pressure (mmHg)');
-
     vis.yAxis = d3.axisLeft()
         .scale(vis.yScale);
 
@@ -65,11 +64,11 @@ BarChart.prototype.initVis = function () {
         .attr('class', 'd3-tip')
         .offset([-10,0])
         .html(function(d) {
-        return "<strong>Country:</strong> <span style='color:red'>" + d.country + "</span><br>" +
-            "<strong>Prevalence to Physical Inactivity:</strong> <span style='color:red'>" + d.physicallyInactive + "%" +  "</span><br>" +
-            "<strong>Prevalence to Overweight:</strong> <span style='color:red'>" + d.overweight + "%" +  "</span><br>" +
-            "<strong>Prevalence to Obesity:</strong> <span style='color:red'>" + d.obese + "%" +  "</span>";
+            return "<strong>Country:</strong> <span style='color: "+continentColor(d.region)+"'>" + d.country + "</span><br>" +
+                   "<strong>Prevalence of "+ vis.textFriendlyCategories[vis.selectedCategory] +": <span style='color:red'>" + d[vis.selectedCategory] + "%" +  "</span><br>";
     });
+
+    vis.svg.call(vis.tooltip);
 
     vis.updateVis();
 };
@@ -85,8 +84,6 @@ BarChart.prototype.updateVis = function() {
 
     vis.xScale.domain(vis.filteredData.map(d => d.country));
     vis.yScale.domain([0, d3.max(vis.filteredData, d => d[vis.selectedCategory])]);
-
-    vis.svg.call(vis.tooltip);
 
     vis.bars = vis.svg.selectAll(".bar")
         .data(vis.filteredData, d => d.country);
@@ -139,175 +136,11 @@ BarChart.prototype.selectCategory = function(newCategory) {
     vis.updateVis();
 };
 
-// SVG drawing area
 
-// var marginP = {top: 40, right: 10, bottom: 60, left: 60};
-
-// var widthP = 960 - marginP.left - marginP.right,
-//     heightP = 500 - marginP.top - marginP.bottom;
-
-// var svgP = d3.select("#chart-area-9").append("svg")
-//     .attr("width", widthP + marginP.left + marginP.right)
-//     .attr("height", heightP + marginP.top + marginP.bottom)
-//     .append("g")
-//     .attr("transform", "translate(" + marginP.left + "," + marginP.top + ")");
-
-
-// Scales
-// var xP = d3.scaleBand()
-//     .range([0, widthP])
-//     .paddingInner(0.1);
-
-// var yP = d3.scaleLinear()
-//     .range([heightP, 0]);
-
-// Tooltip
-
-// var regionColor = d3.scaleOrdinal()
-//     .domain(['Asia', 'Americas', 'Africa', 'Europe', 'Oceania'])
-//     .range(['#e41a1c', '#ff7f00', '#4daf4a', '#377eb8', '#984ea3']);
-
-// Axis
-// var xAxisP = d3.axisBottom()
-//     .scale(xP);
-//
-// var yAxisP = d3.axisLeft()
-//     .scale(yP);
-
-//var xAxisGroup = svgP.append("g")
-//    .attr("class", "x-axis axis")
-//    .attr("transform", "translate(0," + heightP + ")")
-
-// var yAxisGroup = svgP.append("g")
-//     .attr("class", "y-axis axis");
-//
-// var yAxisTitle = svgP.append("text")
-//     .attr("class", "axis-title")
-//     .attr("text-anchor", "middle")
-//     .attr("y", -10)
-//     .attr("x", 0);
-
-
-// Initialize data
-// loadData();
-//
-// // Create a 'data' property under the window object
-// // to store the coffee chain data
-// Object.defineProperty(window, 'data', {
-//     // data getter
-//     get: function() { return _data; },
-//     // data setter
-//     set: function(value) {
-//         _data = value;
-//         // update the visualization each time the data property is set by using the equal sign (e.g. data = [])
-//         updateVisualization()
-//     }
-// });
-
-// Sort order
-// var reverse = false;
-
-// Event Listener (ranking type)
-// var selectRankingType = d3.select("#ranking-type").on("change", updateVisualization);
-
-
-// Event listener (reverse sort order)
-// var changeSortingOrder = d3.select("#change-sorting").on("click", function() {
-//     reverse = !reverse;
-//     updateVisualization();
-// });
-
-// Load CSV file
-// function loadData() {
-//     d3.csv("data/prevalence-overweight-obese-physical-activity.csv", function(error, csv) {
-//
-//         csv.forEach(function(d){
-//             d.Obese = +d.Obese;
-//             d.Overweight = +d.Overweight;
-//             d.Physical_inactivity = +d.Physical_inactivity;
-//             d.Region = d.Region;
-//         });
-//
-//         // Store csv data in global variable
-//         // data = csv;
-//
-//         // updateVisualization gets automatically called within the data = csv call;
-//         // basically(whenever the data is set to a value using = operator);
-//         // see the definition above: Object.defineProperty(window, 'data', { ...
-//     });
-// }
-
-// Render visualization
-function updateVisualization() {
-    // Get the selected ranking option
-    // var rankingType = selectRankingType.property("value");
-    //
-    // if(rankingType == "Overweight")
-    //     yAxisTitle.text("Prevalence (%)");
-    // else if (rankingType == "Obese")
-    //     yAxisTitle.text("Prevalence (%)");
-    // else
-    //     yAxisTitle.text("Prevalence (%)");
-    //
-    // // Sort data
-    // data.sort(function(a, b) { return b[rankingType] - a[rankingType]; });
-    //
-    // if(reverse)
-    //     data.reverse();
-
-    // Update scales domains
-    // xP.domain(data.map(function(d) { return d.Country; }));
-    // yP.domain([0, d3.max(data, function(d) { return d[rankingType]; })]);
-
-    // var tip = d3.tip()
-    //     .attr('class', 'd3-tip')
-    //     .offset([-10, 100
-
-    // svgP.call(tip);
-
-    // Data join
-    // var bars = svgP.selectAll(".bar")
-    //     .data(data, function(d){ return d.Country; });
-
-    // Enter
-    // bars.enter().append("rect")
-    //     .attr("height",0)
-    //     .attr("y",heightP)
-    //     .attr("class", "bar")
-    //     .attr('fill', d => regionColor(d.Region))
-    //
-    //     // Update
-    //     .merge(bars)
-    //     .style("opacity", 0.5)
-    //     .transition()
-    //     .duration(1000)
-    //     .style("opacity", 1)
-    //     .attr("x", function(d) { return xP(d.Country); })
-    //     .attr("y", function(d) { return yP(d[rankingType]); })
-    //     .attr("width", xP.bandwidth())
-    //     .attr("height", function(d) { return heightP - yP(d[rankingType]); });
-
-    // svgP.selectAll(".bar")
-    //     .data(data)
-    //     .enter().append("rect")
-    //     .attr("x", function(d) { return xP(d.Country); })
-    //     .attr("y", function(d) { return yP(d[rankingType]); })
-    //     .attr("width", xP.bandwidth())
-    //     .attr("height", function(d) { return heightP - yP(d[rankingType]); })
-    //     .on('mouseover', tip.show)
-    //     .on('mouseout', tip.hide);
-
-
-    // Exit
-    // bars.exit().remove();
-
-    // Draw Axes
-    // xAxisGroup = svgP.select(".x-axis")
-    //     .attr("transform", "translate(0," + heightP + ")")
-    //     .transition()
-    //     .duration(1000)
-    //     .call(xAxisP);
-    //
-    // yAxisGroup = svgP.select(".y-axis")
-    //     .call(yAxisP);
-}
+BarChart.prototype.filterData = function() {
+    let vis = this;
+    vis.filteredData = vis.data.filter(d =>
+        (vis.mapLegend.selectedRegions.length === 0 || vis.mapLegend.selectedRegions.includes(d.region))
+    );
+    vis.updateVis();
+};
